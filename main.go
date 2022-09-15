f09f4aafa3aa1fd05a6274cd051a10b08b2f462d
maheshmule-coforge <mahesh.1.mule@coforge.com>
2022-09-15 18:08:52 +0530

    Golang training assessment enhancement

--- main.go
+++ main.go
@@ -7,52 +7,92 @@ import (
 )
 
 type User struct {
-	Id       int64 `json:"userId"`
-	Password int64 `json:"Password"`
+	Id       int64
+	Password int64
+	Name     string
+	Balance  float64
+}
+
+type Users struct {
+	Users []User `json:"users"`
 }
 
 var selectedOption string
 var userId int64
 var password int64
+var loggedUser User
 
 func login() {
 	var option string
-	fmt.Println("Please enter your user id: ")
-	fmt.Scan(&userId)
-	fmt.Println("Please enter your password: ")
-	fmt.Scan(&password)
-	user := User{Id: userId, Password: password}
+	var bal float64 = 0
+	var userData Users
+	if loggedUser.Id < 1 {
 
-	f, err := os.Open("user.json")
-	if err != nil {
-		fmt.Println("Error while validating user")
-	}
-	defer f.Close()
+		fmt.Println("Please enter your user id: ")
+		fmt.Scan(&userId)
+		fmt.Println("Please enter your password: ")
+		fmt.Scan(&password)
+		user := User{Id: userId, Password: password}
 
-	var userData User
-	err = json.NewDecoder(f).Decode(&userData)
-	if err != nil {
-		fmt.Println("Error in reading file")
-	}
+		f, err := os.Open("user.json")
+		if err != nil {
+			fmt.Println("Error while validating user")
+		}
+		defer f.Close()
 
-	if userData.Id == user.Id && userData.Password == user.Password {
-		fmt.Println("User authenticated successfully...")
-		fmt.Println("1.Withdraw money.\n2.Deposit money.\n3.Request balance.\n4.Quit the program.")
-		fmt.Scan(&option)
-	} else {
-		fmt.Println("\nIncorrect UserId or Password \n")
-		main()
+		err = json.NewDecoder(f).Decode(&userData)
+		if err != nil {
+			fmt.Println("Error in reading file")
+		}
+		var isValid bool = false
+
+		for i, _ := range userData.Users {
+			if userData.Users[i].Id == user.Id && userData.Users[i].Password == user.Password {
+				loggedUser = userData.Users[i]
+				isValid = true
+			}
+		}
+		if isValid {
+			fmt.Println("User authenticated successfully...")
+			fmt.Println("1.Withdraw money.\n2.Deposit money.\n3.Request balance.\n4.Quit the program.")
+			fmt.Scan(&option)
+		} else {
+			fmt.Println("\nIncorrect UserId or Password \n")
+			main()
+		}
 	}
 	switch option {
 	case "1":
-		fmt.Println("Please withdraw money")
-		main()
+		fmt.Println("Please enter amount to withdraw money")
+		fmt.Scan(&bal)
+		loggedUser.Balance -= bal
+		for i, _ := range userData.Users {
+			if userData.Users[i].Id == loggedUser.Id {
+				userData.Users[i].Balance = loggedUser.Balance
+			}
+		}
+		content, _ := json.Marshal(userData)
+		//fmt.Println(string(content))
+		os.WriteFile("user.json", content, 0644)
+		bal = 0
+		login()
 	case "2":
-		fmt.Println("Please deposit money")
-		main()
+		fmt.Println("Please enter amount to deposit money: ")
+		fmt.Scan(&bal)
+		loggedUser.Balance += bal
+		for i, _ := range userData.Users {
+			if userData.Users[i].Id == loggedUser.Id {
+				userData.Users[i].Balance = loggedUser.Balance
+			}
+		}
+		content, _ := json.Marshal(userData)
+		//fmt.Println(string(content))
+		os.WriteFile("user.json", content, 0644)
+		bal = 0
+		login()
 	case "3":
-		fmt.Println("Please check balance")
-		main()
+		fmt.Println("Your Balance: ", loggedUser.Balance)
+		login()
 	case "4":
 		fmt.Println("exiting...")
 		os.Exit(3)
@@ -65,7 +105,26 @@ func createAccount() {
 	fmt.Scan(&newUser.Id)
 	fmt.Println("Please enter password:")
 	fmt.Scan(&newUser.Password)
+	fmt.Println("Please enter name:")
+	fmt.Scan(&newUser.Name)
+	newUser.Balance = 100
 	fmt.Println("User Created..", newUser)
+
+	f, err := os.Open("user.json")
+	if err != nil {
+		fmt.Println("Error while validating user")
+	}
+	defer f.Close()
+
+	var userData Users
+	err = json.NewDecoder(f).Decode(&userData)
+	if err != nil {
+		fmt.Println("Error in reading file")
+	}
+	userData.Users = append(userData.Users, newUser)
+	content, _ := json.Marshal(userData)
+	fmt.Println(string(content))
+	os.WriteFile("user.json", content, 0644)
 }
 
 func main() {
