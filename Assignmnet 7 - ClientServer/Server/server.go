ba852812ca54cd5e0af9f9f450b71e46a2f557c3
maheshmule-coforge <mahesh.1.mule@coforge.com>
2022-09-27 10:38:36 +0530

    used map for client list

--- Assignmnet 7 - ClientServer/Server/server.go
+++ Assignmnet 7 - ClientServer/Server/server.go
@@ -35,6 +35,8 @@ var counter = 0
 var clientList []string
 
 func main() {
+	clients := make(map[int]net.Conn)
+
 	fmt.Println(" strating server...")
 	ln, err := net.Listen("tcp", port)
 
@@ -50,11 +52,14 @@ func main() {
 		}
 		if isPresent == false {
 			counter += 1
+			clients[counter] = conn
 			clientList = append(clientList, conn.RemoteAddr().String())
 		}
-
-		fmt.Println(" Connected Client : ", conn.RemoteAddr())
+		for i, _ := range clients {
+			fmt.Println(" Connected Client : ", string(clients[i].RemoteAddr().String()))
+		}
 		fmt.Println(" Number of Clients Connected : ", counter)
+
 		if err != nil {
 			fmt.Print(" Error", err)
 			continue
